18ed67bfaaff742f8af0b9703990520eca8f583b
ad3kola <129291078+ad3kola@users.noreply.github.com>
2025-02-16 01:43:45 -0400

    new commit

--- components/Dashboard/GradientChart.tsx
+++ components/Dashboard/GradientChart.tsx
@@ -18,12 +18,12 @@ import {
   ChartTooltipContent,
 } from "@/components/ui/chart"
 const chartData = [
-  { month: "January", desktop: 186, mobile: 80 },
-  { month: "February", desktop: 305, mobile: 200 },
+  { month: "January", desktop: 200, mobile: 430 },
+  { month: "February", desktop: 3045, mobile: 200 },
   { month: "March", desktop: 237, mobile: 120 },
-  { month: "April", desktop: 73, mobile: 190 },
-  { month: "May", desktop: 209, mobile: 130 },
-  { month: "June", desktop: 214, mobile: 140 },
+  { month: "April", desktop: 405, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 594 },
+  { month: "June", desktop: 490, mobile: 494 },
 ]
 
 const chartConfig = {
